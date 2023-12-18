ec91fdaeea296039dbb3c0576a4eaf98db1ab0cb
Ishitav03 <vishira383@gmail.com>
2023-12-19 02:06:38 +0530

    Modified tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: "#1182c5",
+        secondary: "#2aa6df",
+        dark: "#1e1e1e",
+        light: "#f5f5f5",
+        navbg: "#D3DCE7",
+        navyblue: "#486284",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+      animation: {
+        "spin-slow": "spin 40s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
